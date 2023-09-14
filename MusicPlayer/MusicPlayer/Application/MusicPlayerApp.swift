a1fafcd036377d2c97d627a7730286c2603a233a
Rafael Nunes <rafaelnunex@hotmail.com>
2023-09-13 23:25:23 -0300

    Project setup #master [setup]

---
+++ MusicPlayer/MusicPlayer/Application/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Rafael Nunes Bezerra Dias on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
